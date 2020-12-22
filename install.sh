2b20ac52029cf9724885fd9b0a8d8c2affd5455a
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-22 16:59:35 +0100

    housekeeping

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@
 echo "Creating Sonarqube namespace"
 oc apply -f sonarqube-namespace.yaml
 echo "Sonarqube namespace created!"
-
+echo " "
 echo "Deploying Sonarqube"
 oc apply -f sonarqube-template.yaml -n sonarqube
 echo "Sonarqube deployed!"
-
-echo "Searching for available routes"
-oc get routes -n sonarqube
-echo "connect to the route named **sonarqube** using your browser \n
-and login using credentials (username/password: admin/admin)"
+echo " "
+#echo "Searching for available routes"
+#oc get routes -n sonarqube
+#echo "connect to the route named **sonarqube** using your browser \n
+#and login using credentials (username/password: admin/admin)"
